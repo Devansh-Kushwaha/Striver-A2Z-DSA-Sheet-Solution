eed44dae65e6c4a3532538662625d3490959c565
Devansh Kushwaha <127891968+Devansh-Kushwaha@users.noreply.github.com>
2025-07-01 14:13:14 +0530

    Create solution.py

---
+++ Two Sum/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        from collections import defaultdict
+        d=defaultdict(int)
+        for i in range(len(nums)):
+            if target-nums[i] in d and d[target-nums[i]]!=i:
+                print(nums[i])
+                return [d[target-nums[i]],i]
+            d[nums[i]]=i
+            
+        return []
+        
